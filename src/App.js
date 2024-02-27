d880992fee1c8ed6e803c13c959fa1ec9b935980
BhanuThapa <bhanuthapa252@gmail.com>
2024-02-27 21:39:00 +0530

    testing branch

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello Saloon App , the first commit
-      let me test the branches
+      Hello Saloon App , the first commit let me test the branches
+      <br />
+      Bhanu Checking Branch
     </div>
   );
 }
