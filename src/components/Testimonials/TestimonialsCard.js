cfb2a2cecfedf6f7a63c38f821731e98773d6c13
BhanuThapa <bhanuthapa252@gmail.com>
2024-03-12 16:05:33 +0530

    Teatimonials Section Meadia Query

--- src/components/Testimonials/TestimonialsCard.js
+++ src/components/Testimonials/TestimonialsCard.js
@@ -1,21 +1,71 @@
+import React, { useState, useEffect } from 'react';
 import double from '../../image/testimonials/double.png';
 import { IoIosStar } from 'react-icons/io';
 
 function TestimonialsCards({ objectData, value }) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const getCardStyles = (index) => {
+    let styles = {
+      left: `${index * 340}px`,
+      transform: `translateX(-${value}px)`,
+    };
+
+    // Adjust styles for tablets (max-width: 1200px)
+    if (windowWidth <= 1200) {
+      styles.left = `${index * 310}px`;
+      styles.transform = `translateX(-${value}px)`;
+    }
+
+    // Adjust styles for small tab (max-width: 768px)
+    if (windowWidth <= 768) {
+      styles.left = `${index * 280}px`;
+      styles.transform = `translateX(-${value}px)`;
+    }
+    // Adjust styles for mobile (max-width: 564px)
+    if (windowWidth <= 564) {
+      styles.left = `${index * 280}px`;
+      styles.transform = `translateX(-${value}px)`;
+    }
+
+    return styles;
+  };
+
   return (
     <>
       {objectData.map((data, index) => (
         <div
           className="testimonials-card"
-          style={{
-            left: `${index * 340}px`,
-            transform: `translateX(-${value}px)`,
+          // style={{
+          //   left: `${index * 340}px`,
+          //   transform: `translateX(-${value}px)`,
 
-            ...(window.innerWidth <= 1200 && {
-              left: `${index * 310}px`,
-              transform: `translateX(-${value - 100}px)`,
-            }),
-          }}
+          ////// DIRECT MEDIA QUERY NOT WORKING  //////
+          //   // Responsive styling for screens smaller than or equal to 1200px (tablet)
+          //   '@media screen and (max-width: 1200px)': {
+          //     left: `${index * 210}px`,
+          //     transform: `translateX(-${value - 100}px)`,
+          //   },
+
+          //   // Responsive styling for screens smaller than or equal to 768px (mobile)
+          //   '@media screen and (max-width: 768px)': {
+          //     left: `${index * 280}px`,
+          //     transform: `translateX(-${value - 150}px)`,
+          //   },
+          // }}
+          style={getCardStyles(index)}
         >
           <div className="testimonials-pic">
             <img src={data.image} alt="nick" />
