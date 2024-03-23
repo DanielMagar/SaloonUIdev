9c31f851a067679ecb2797c96aba8cd1216e1a37
BhanuThapa <bhanuthapa252@gmail.com>
2024-03-23 17:05:43 +0530

    added services carousel

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ServicesCard from './ServicesCard';
 import services from './servicesData';
 
-// Image from Firebase storage
-
 function Services() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const change1 = () => setCurrentPage(1);
+  const change2 = () => setCurrentPage(2);
+
+  const startIndex = (currentPage - 1) * 4;
+  const visibleServices = services.slice(startIndex, startIndex + 4);
+
   return (
     <div className="services-sec">
       <h2>Services</h2>
 
       <div className="services-outer-container">
-        {services.map((data) => (
+        {visibleServices.map((data) => (
           <ServicesCard
             image={data.image}
             name={data.service[0].name}
             servicesList={data.service[1].servicesList}
           />
         ))}
-        {/* <ServicesCard image={makeup} name="makeup" />
-        <ServicesCard image={manicure} name="manicure" />
-        <ServicesCard image={color} name="color" />
-        <ServicesCard image={color} name="color" /> */}
       </div>
       <div className="dots">
-        <div className="dot dot1"></div>
-        <div className="dot dot2"></div>
+        <div
+          className="dot"
+          style={{
+            backgroundColor: currentPage === 1 ? 'rgb(247, 51, 165)' : '',
+          }}
+          onClick={change1}
+        ></div>
+        <div
+          className="dot"
+          style={{
+            backgroundColor: currentPage === 2 ? 'rgb(247, 51, 165)' : '',
+          }}
+          onClick={change2}
+        ></div>
       </div>
     </div>
   );
--- src/components/Services/servicesData.js
+++ src/components/Services/servicesData.js
@@ -78,22 +78,53 @@ const services = [
       },
     ],
   },
+  {
+    id: 1,
+    image: cut,
+    service: [
+      { name: 'Cut2' },
+      {
+        servicesList: [
+          { id: 1, name: 'Short Hair Cut', price: 400 },
+          { id: 2, name: 'Long Hair Cut', price: 500 },
+          { id: 3, name: 'Bangs', price: 300 },
+          { id: 4, name: 'Cut + Blow dry', price: 600 },
+        ],
+      },
+    ],
+  },
 
-  // {
-  //   id: 2,
-  //   image: makeup,
-  //   service: [
-  //     { name: 'Makeup' },
-  //     {
-  //       servicesList: [
-  //         { id: 1, name: 'Wedding Make-up', price: 600 },
-  //         { id: 2, name: 'Make-up & Lashes', price: 450 },
-  //         { id: 3, name: 'Party Glam', price: 500 },
-  //         { id: 4, name: 'Glamurous', price: 550 },
-  //       ],
-  //     },
-  //   ],
-  // },
+  {
+    id: 2,
+    image: makeup,
+    service: [
+      { name: 'Makeup2' },
+      {
+        servicesList: [
+          { id: 1, name: 'Wedding Make-up', price: 600 },
+          { id: 2, name: 'Make-up & Lashes', price: 450 },
+          { id: 3, name: 'Party Glam', price: 500 },
+          { id: 4, name: 'Glamurous', price: 550 },
+        ],
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    image: manicure,
+    service: [
+      { name: 'manicure2' },
+      {
+        servicesList: [
+          { id: 1, name: 'Classic Manicure', price: 400 },
+          { id: 2, name: 'French Manicure', price: 500 },
+          { id: 3, name: 'Press-On Nails', price: 300 },
+          { id: 4, name: 'Gel Manicure', price: 550 },
+        ],
+      },
+    ],
+  },
 ];
 
 export default services;
